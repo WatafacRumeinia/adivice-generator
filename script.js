7bc62ff2b190e851d3f013bc104805e4105dcaad
Neculaiu Cosmin <80151812+WatafacRumeinia@users.noreply.github.com>
2022-02-28 22:02:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,24 +16,3 @@ btn.addEventListener("click",()=>{
     })
 })
 
-// "use strict";
-
-// const diceBtn = document.querySelector(".dice-container");
-// const adviceNumber = document.querySelector(".advice");
-// const adviceText = document.querySelector(".quotes");
-// const apiUrl = "https://api.adviceslip.com/advice";
-
-// //Button click event
-// diceBtn.addEventListener("click", () => {
-//   //Request Data
-//   fetch(apiUrl, {cache: "no-cache"})
-//     .then((response) => response.json())
-//     .then((response) => {
-//       let data = response.slip;
-//       let dataId = data.id;
-//       let dataAdvice = data.advice;
-//       //Inject to DOM
-//       adviceNumber.innerHTML = `advice # ${dataId}`;
-//       adviceText.innerHTML = dataAdvice;
-//     });
-// });
